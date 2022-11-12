69e11d8e745ce6d4bd6974ec4425bb0c30f895ee
KarlLundengaard <106549720+KarlLundengaard@users.noreply.github.com>
2022-11-12 21:12:21 +0000

    Adjusted numerical check parameters

--- app/perf_test.py
+++ app/perf_test.py
@@ -3,14 +3,19 @@ try:
 except ImportError:
     from evaluation import evaluation_function, parse_error_warning
 
-n = 100
+n = 10
 for k in range(0,n):
     params = {"strict_syntax": False}
-    answer = "6*cos(5*x+1)-90*x*sin(5*x+1)-225*x**2*cos(5*x+1)+125*x**3*sin(5*x+1)"
+    answer = "-(y*sin(x*y) + e^(y)) / (x*(e^(y) + sin(x*y)))"
     responses = [
-        #"6cos(5x+1)-90x*sin(5x+1)-225x^2cos(5x+1)+125x^3sin(5x+1)" # Correct answer
-        "6cos(5x+1)-90xsin(5x+1)-225x^2cos(5x+1)+125x^3sin(5x+1)", # Wrong answer (due to issues with implicit multiplication)
+        "-(y*sin(x*y) + exp(y)) / (x*(exp(y) + sin(x*y)))",
+        #"-(sin(xy)y+(e^y))/(x(e^y+sin(xy)x))"
         ]
+    #answer = "6*cos(5*x+1)-90*x*sin(5*x+1)-225*x**2*cos(5*x+1)+125*x**3*sin(5*x+1)"
+    #responses = [
+    #    #"6cos(5x+1)-90x*sin(5x+1)-225x^2cos(5x+1)+125x^3sin(5x+1)" # Correct answer
+    #    "6cos(5x+1)-90xsin(5x+1)-225x^2cos(5x+1)+125x^3sin(5x+1)", # Wrong answer (due to issues with implicit multiplication)
+    #    ]
     for response in responses:
         result = evaluation_function(response, answer, params)
         print(result["is_correct"])
